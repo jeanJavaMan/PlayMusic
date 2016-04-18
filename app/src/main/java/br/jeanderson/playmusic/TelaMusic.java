f54e0890f6e5f66ff7a958816b4dd401421980ac
Jeanderson Silva Lopes <jeananimax@gmail.com>
2016-04-18 10:17:21 -0300

    Utilizando o GLIDE

--- app/src/main/java/br/jeanderson/playmusic/TelaMusic.java
+++ app/src/main/java/br/jeanderson/playmusic/TelaMusic.java
@@ -3,17 +3,20 @@ package br.jeanderson.playmusic;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import br.jeanderson.banco.Configuracao;
 import br.jeanderson.task.Funcao;
 import br.jeanderson.task.ListaTask;
+import br.jeanderson.util.GlideTarget;
 
 public class TelaMusic extends AppCompatActivity implements AdapterView.OnItemClickListener, View.OnClickListener {
     private TextView txtTocando;
@@ -22,6 +25,7 @@ public class TelaMusic extends AppCompatActivity implements AdapterView.OnItemCl
     private ListaTask task;
     private Funcao funcao;
     private int contador;
+    private RelativeLayout telaMusicas;
     private Configuracao config;
 
     @Override
@@ -33,6 +37,7 @@ public class TelaMusic extends AppCompatActivity implements AdapterView.OnItemCl
         this.btnDiminuir = (Button) findViewById(R.id.btnDiminuir);
         this.btnParar = (Button) findViewById(R.id.btnParar);
         this.lvMusicas = (ListView) findViewById(R.id.lvMusicas);
+        this.telaMusicas = (RelativeLayout) findViewById(R.id.TelaMusicas);
         this.lvMusicas.setOnItemClickListener(this);
         this.task = new ListaTask(lvMusicas, this);
         this.task.execute();
@@ -45,13 +50,11 @@ public class TelaMusic extends AppCompatActivity implements AdapterView.OnItemCl
         config = new Configuracao(this);
         try {
             String ipServer = config.getIpServer();
-            this.funcao = new Funcao(ipServer);
+            this.funcao = new Funcao(ipServer,this);
         } catch (NullPointerException ex) {
-            ex.printStackTrace();
-            Log.i("Porra","Null");
             Snackbar.make(lvMusicas, "Nenhum IP configurado, por favor configure um IP antes!", Snackbar.LENGTH_LONG).show();
         }
-
+        Glide.with(this).load(R.drawable.iurd_fundo).asBitmap().into(new GlideTarget(this,telaMusicas));
     }
 
     @Override
--- app/src/main/java/br/jeanderson/playmusic/TelaPrincipal.java
+++ app/src/main/java/br/jeanderson/playmusic/TelaPrincipal.java
@@ -6,12 +6,17 @@ import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.bumptech.glide.Glide;
 
 import br.jeanderson.dialog.DialogIP;
+import br.jeanderson.util.GlideTarget;
 
 public class TelaPrincipal extends AppCompatActivity implements View.OnClickListener {
     private Button btnMusic, btnConfig;
     private DialogIP dialogIP;
+    private RelativeLayout telaPrincipal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +26,11 @@ public class TelaPrincipal extends AppCompatActivity implements View.OnClickList
         StrictMode.setThreadPolicy(policy);
         this.btnMusic = (Button) findViewById(R.id.btnMusic);
         this.btnConfig = (Button) findViewById(R.id.btnConfig);
+        telaPrincipal = (RelativeLayout) findViewById(R.id.TelaPrincipal);
         btnMusic.setOnClickListener(this);
         btnConfig.setOnClickListener(this);
         this.dialogIP = new DialogIP(this);
+        Glide.with(this).load(R.drawable.iurd_fundo).asBitmap().into(new GlideTarget(this,telaPrincipal));
     }
 
     @Override
